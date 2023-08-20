d9a3c201048bbc2497444384e6496c75c93bb6cb
Rajveersinh Gohil <140502093+Rajveer-CSE-27@users.noreply.github.com>
2023-08-21 04:38:10 +0530

    Create adjacencyList.cpp

---
+++ Graph/adjacencyList.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void addEdge(int vertex1, int vertex2, int vertices, vector<int> graph[]) {
+	if(vertex1 < 0 or vertex1 >= vertices or vertex2 < 0 or vertex2 >= vertices) {
+			cout << "Index out of bound";
+			return;
+	}
+	
+	graph[vertex1].push_back(vertex2);
+	graph[vertex2].push_back(vertex1);
+}
+
+void removeEdge(int vertex1, int vertex2, int vertices, vector<int> graph[]) {
+	if(vertex1 < 0 or vertex1 >= vertices or vertex2 < 0 or vertex2 >= vertices) {
+			cout << "Index out of bound";
+			return;
+	}
+	auto iterator1 = graph[vertex1].begin();
+	auto iterator2 = graph[vertex2].begin();
+	
+	while(iterator1 != graph[vertex1].end()) {
+		if(*iterator1 == vertex2) {
+			break;
+		}
+		iterator1++;
+	}
+	while(iterator2 != graph[vertex2].end()) {
+		if(*iterator2 == vertex1) {
+			break;
+		}
+		iterator2++;
+	}
+	
+	if(iterator1 != graph[vertex1].end()) {
+		graph[vertex1].erase(iterator1, ++iterator1);
+		graph[vertex2].erase(iterator2, ++iterator2);
+	}
+	
+} 
+
+void printGraph(int vertices, vector<int> graph[]) {
+	for(int i=0; i<vertices; ++i) {
+		cout << i << ": ";
+		for(auto it:graph[i]) {
+			cout << it << " ";
+		}
+		cout << endl;
+	}
+}
+
+int main() {
+	
+	int vertices = 5;
+	
+	vector<int> graph[vertices];
+	addEdge(0,1,vertices,graph);
+	addEdge(0,2,vertices,graph);
+	addEdge(2,3,vertices,graph);
+	addEdge(3,1,vertices,graph);
+	addEdge(4,1,vertices,graph);
+	addEdge(4,3,vertices,graph);
+	
+	printGraph(vertices, graph);
+	cout << endl;
+	
+	removeEdge(4,3,vertices,graph);
+	printGraph(vertices, graph);
+
+	
+	return 0;
+}
